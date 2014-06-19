0f720de51d9cf77a508b053c5d79c4ef34ca4644
guaii <guai-naka@hotmail.com>
2014-06-19 14:17:45 +0700

    Modifield CarLicense

--- api/src/main/java/api/dao/CarLicenseDAO.java
+++ api/src/main/java/api/dao/CarLicenseDAO.java
@@ -14,11 +14,11 @@ public class CarLicenseDAO {
 
 	// JDBC driver name and database URL
 	final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	final String DB_URL = "jdbc:mysql://192.168.100.222/PARK_KO";
+	final String DB_URL = "jdbc:mysql://192.168.100.222:3306/PARK_KO";
 
 	// Database credentials
 	final String USER = "park-ko";
-	final String PASS = "xitgmlwmp";
+	final String PASS = "xitgmLwmp";
 
 	public List<Province> getProvince() throws SQLException {
 		List<Province> provinceList = new ArrayList<Province>();
@@ -32,9 +32,18 @@ public class CarLicenseDAO {
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			stmt = conn.createStatement();
 			String sql;
-			sql = "select * from PKO_PROVINCE ORDER BY CONVERT(province_name USING TIS620)";
+			sql = "select * from PKO_PROVINCE ORDER BY province_name";
 			rs = stmt.executeQuery(sql);
-			System.out.println("result size " + rs.getFetchSize());
+			
+			while(rs.next()){
+				Province province = new Province();
+				
+				province.setProvinceid(rs.getInt("province_id"));
+				province.setProvincename(rs.getString("province_name"));
+				provinceList.add(province);
+			}
+			
+			System.out.println("ProvinceListSize:" + provinceList.size());
 
 		} catch (Exception ex) {
 			System.out.println("ERROR:" + ex.toString());
--- api/src/main/java/api/servlet/CarLicenseInitServlet.java
+++ api/src/main/java/api/servlet/CarLicenseInitServlet.java
@@ -23,54 +23,17 @@ public class CarLicenseInitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	CarLicenseDAO carLicenseDAO = new CarLicenseDAO();
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CarLicenseInitServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		List<Province> provinceList = getProvince();
-		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writeValue(response.getOutputStream(), provinceList);
-	}
+		try{
+			response.setContentType("text/html; charset=utf-8");
+			List<Province> provinceList = carLicenseDAO.getProvince();
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue(response.getOutputStream(), provinceList);
+		} catch (Exception ex) {
+			System.out.println("ERROR:" + ex.toString());
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-	
-	private List<Province> getProvince(){
-	List<Province> provinceList = new ArrayList<Province>();
-	try{
-		provinceList = carLicenseDAO.getProvince();
-
-	} catch (Exception ex) {
-		System.out.println("ERROR:" + ex.toString());
-
-	}
-	
-	
-//	Province province = new Province();
-//	province.setProvinceid(1);
-//	province.setProvincename("กรุงเทพมหานคร");
-//	provinceList.add(province);
-//	
-//	province.setProvinceid(14);
-//	province.setProvincename("เชียงใหม่");
-//	provinceList.add(province);
-//	
-	return provinceList;
-	}
-	
+		}
+	}	
 }
 
