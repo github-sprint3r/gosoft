b03b5dfc9f7877ec8a15c44eb1716cc3366a3a9f
TheerapongRua <paeman2001@hotmail.com>
2014-06-19 15:35:58 +0700

    modify test case model fee type

--- api/src/test/java/api/model/ModelFeeTypeTests.java
+++ api/src/test/java/api/model/ModelFeeTypeTests.java
@@ -9,30 +9,38 @@ import api.dao.FeeTypeDAO;
 public class ModelFeeTypeTests {
 	
 	@Test
-	public void testInputDAY_TIMEThenReturn1000And0200() throws Exception {
+	public void testInputDAYTIMEThenReturn1000And0200() throws Exception {
 		ModelFeeType modelFeeType = new ModelFeeType();
-		FeeTypeDAOStub feeTypeDAOStub = new FeeTypeDAOStub();
+		FeeTypeDAO feeTypeDAOStub = new FeeTypeDAO (){
+			public void inquiry(ModelFeeType modelFeeType) {
+				modelFeeType.setStartTime("10:00");
+				modelFeeType.setEndTime("02:00");
+			}
+		};
 		modelFeeType.setFeeType("DAY_TIME");
 		modelFeeType.setFeeTypeDAO(feeTypeDAOStub);
 		
 		modelFeeType.inquiry();
-		assertEquals(1,feeTypeDAOStub.getCounter());
 		assertEquals("10:00",modelFeeType.getStartTime());
 		assertEquals("02:00",modelFeeType.getEndTime());
 	}
 	
-}
-
-class FeeTypeDAOStub extends FeeTypeDAO{
-	int counter = 0;
-	public void inquiry(ModelFeeType modelFeeType) {
-		modelFeeType.setStartTime("10:00");
-		modelFeeType.setEndTime("02:00");
-		counter++;
+	@Test
+	public void testInputDAYTIMEThenReturn0201And0959() throws Exception {
+		ModelFeeType modelFeeType = new ModelFeeType();
+		FeeTypeDAO feeTypeDAOStub = new FeeTypeDAO (){
+			public void inquiry(ModelFeeType modelFeeType) {
+				modelFeeType.setStartTime("02:01");
+				modelFeeType.setEndTime("09:59");
+			}
+		};
+		modelFeeType.setFeeType("DAY_TIME");
+		modelFeeType.setFeeTypeDAO(feeTypeDAOStub);
+		
+		modelFeeType.inquiry();
+		assertEquals("02:01",modelFeeType.getStartTime());
+		assertEquals("09:59",modelFeeType.getEndTime());
 	}
 	
-	public int getCounter() {
-		return counter;
-	}
-
 }
+
