d456b80895fc659c57c99216080d030bbd0b7a5a
thongjityua <thongjit.y@live.com>
2014-06-20 11:27:11 +0700

    Change Return Type

--- api/src/main/java/api/dao/FeeTypeDAO.java
+++ api/src/main/java/api/dao/FeeTypeDAO.java
@@ -10,43 +10,39 @@ import api.util.DatabaseUtils;
 
 public class FeeTypeDAO {
 
-	public ModelFeeType inquiry(ModelFeeType modelFeeTypeInput) {
+	public void inquiry(ModelFeeType modelFeeType) {
 
 		Connection connection = null;
 		Statement statement = null;
 		ResultSet resultSet = null;
-		ModelFeeType modelFeeTypeOutput = new ModelFeeType();
-		try {
-			String feeType = modelFeeTypeInput.getFeeType();
 
-			 connection = DatabaseUtils.getConnection();
+		try {
+			String feeType = modelFeeType.getFeeType();
+			connection = DatabaseUtils.getConnection();
 			statement = connection.createStatement();
 			String sql = "SELECT `ID`, `FEE_TYPE`, `FEE_TYPE_NAME`, `START_TIME`, `END_TIME` FROM `PARK_KO`.`PKO_FEE_TYPE` WHERE FEE_TYPE = '"
 					+ feeType + "';";
 			resultSet = statement.executeQuery(sql);
 
 			if (resultSet.next()) {
-				modelFeeTypeOutput.setId(resultSet.getString("ID"));
-				modelFeeTypeOutput.setFeeType(resultSet.getString("FEE_TYPE"));
-				modelFeeTypeOutput.setFeeTypeName(resultSet
+				modelFeeType.setId(resultSet.getString("ID"));
+				modelFeeType.setFeeType(resultSet.getString("FEE_TYPE"));
+				modelFeeType.setFeeTypeName(resultSet
 						.getString("FEE_TYPE_NAME"));
-				modelFeeTypeOutput.setStartTime(resultSet
-						.getString("START_TIME"));
-				modelFeeTypeOutput.setEndTime(resultSet.getString("END_TIME"));
+				modelFeeType.setStartTime(resultSet.getString("START_TIME"));
+				modelFeeType.setEndTime(resultSet.getString("END_TIME"));
 			}
 		} catch (Exception ex) {
 			System.out.println("ERROR:" + ex.toString());
 
 		} finally {
-			 try {
-				DatabaseUtils.releaseResources(connection, statement, resultSet);
+			try {
+				DatabaseUtils
+						.releaseResources(connection, statement, resultSet);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-
-		return modelFeeTypeOutput;
-
 	}
 
 }
